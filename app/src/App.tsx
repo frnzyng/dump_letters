7ebbcadcb3b85aa0e00d572cfe92d8c5288aed3c
Franze Angelei Young <120648811+frnzyng@users.noreply.github.com>
2023-10-14 10:35:20 +0800

    Update App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,7 +5,7 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass';
 import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft';
-import HomePage from './src/Home';
+import HomePage from './Home';
 import SearchPage from './Search';
 import ProfilePage from './Profile';
 import './App.css'
